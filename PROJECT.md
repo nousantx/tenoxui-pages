6220a1d6a4ddafe14bdb0a71a29ffcf29a79ed66
nousantx <nousantx@gmail.com>
2023-12-13 11:59:07 +0700

    Update Design

---
+++ PROJECT.md
@@ -0,0 +1,9 @@
+# Whats version of this day?
+
+## - Day 1 : Tupat Dev [12-Dec-2023]
+
+- TenoxUI v0.1.5
+
+## - Day 1 : Finenl [13-Dec-2023]
+
+- TenoxUI v0.3.0
---
+++ design-day2/style.js
@@ -0,0 +1,10 @@
+import TenoxUI, {
+  MakeProp,
+  MakeStyle,
+  MultiStyles,
+  MultiProps,
+} from "../node_modules/tenoxui/dist/js/index.js";
+
+MultiProps(MyProps);
+MultiStyles(MyStyles);
+TenoxUI();
